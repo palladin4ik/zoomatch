83136216c1dfbda9fd348a8d2ab219c02a5caf7e
Vyacheslav Topilski <topilski.vyacheslav04@gmail.com>
2025-11-06 23:38:34 +0300

    Initial base setup and user model creation.

---
+++ ZooMatch/users/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        BaseUserManager, PermissionsMixin)
+from django.db import models
+from django.utils import timezone
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    avatar = models.FilePathField(blank=True, null=True)
+    location = models.TextField()
+    phone_number = models.CharField(
+        max_length=20,
+        unique=True,
+        blank=True, null=True)
+    role = models.PositiveSmallIntegerField(default=0)
+    chats = models.TextField()  # Many to Many с чатами
+    last_seen = models.DateTimeField(default=timezone.now)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
