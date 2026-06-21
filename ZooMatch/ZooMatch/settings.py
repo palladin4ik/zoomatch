2287350d8aeeff7294a194a7e0509e01cb68cfc3
Vyacheslav Topilski <topilski.vyacheslav04@gmail.com>
2026-06-21 15:36:19 +0300

    Web prod.

--- ZooMatch/ZooMatch/settings.py
+++ ZooMatch/ZooMatch/settings.py
@@ -109,9 +109,9 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
--- zoomatch-web/vite.config.js
+++ zoomatch-web/vite.config.js
@@ -1,23 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue: 'vue/dist/vue.esm-bundler.js',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      {
+        name: 'html-env',
+        transformIndexHtml(html) {
+          return html.replace(
+            /%VITE_YANDEX_MAPS_API_KEY%/g,
+            env.VITE_YANDEX_MAPS_API_KEY
+          )
+        }
+      }
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        vue: 'vue/dist/vue.esm-bundler.js',
+      },
+    },
+  }
 })
